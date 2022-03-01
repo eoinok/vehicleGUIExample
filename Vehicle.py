315432ee93ac9e337d66c40affea2b98e82196d3
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2022-03-01 14:04:26 +0000

    added vehicle class

---
+++ Vehicle.py
@@ -0,0 +1,18 @@
+class Vehicle():
+    def __init__(self,arg1,arg2,arg3,arg4):
+        self.__regNum=arg1
+        self.__make=arg2
+        self.__model=arg3
+        self.__cO2_emissions
+
+    def get_cO2_emissions(self):
+        return self.__cO2_emissions
+
+    def get_annual_tax(self):
+        tax = 150
+        if (self.__cO2_emissions <= 90):
+            tax = 100
+        return tax
+
+    def __str__(self):
+        return self.__regNum + " " + self.__make + " " + self.__model
